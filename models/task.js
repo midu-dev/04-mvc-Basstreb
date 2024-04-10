44ab0f9e665d321d38341c9dc908b99554db5dc4
Daniel Lozano Campano <danlozcam@gmail.com>
2024-04-10 20:43:15 +0200

    feat: finished exercise

--- models/task.js
+++ models/task.js
@@ -15,28 +15,49 @@ let currentId = 1
  * @class
  */
 
-/**
- * export class TaskModel {
- *
- *
+export class TaskModel {
   /**
    * Fetches all tasks.
    * @name TaskModel.getAllTasks
    * @static
    * @returns {Array<Object>} Array of tasks.
    */
+  static getAllTasks () {
+    return tasks
+  }
 
-/**
+  /**
    * Adds a new task to the list.
    * @name TaskModel.addTask
    * @static
    * @param {string} description - The description of the task.
    * @returns {Object} The new task object.
    */
+  static addTask (description) {
+    const newTask = {
+      id: currentId++,
+      description
+    }
+    tasks.push(newTask)
 
-/**
+    return newTask
+  }
+
+  /**
    * Deletes a task from the list by its ID.
    * @name TaskModel.deleteTask
    * @static
    * @param {number} id - The ID of the task to delete.
    */
+  static deleteTask (id) {
+    const taskIndex = tasks.findIndex((task) => task.id === Number(id))
+
+    if (taskIndex >= 0) {
+      tasks.splice(taskIndex, 1)
+
+      return tasks
+    }
+
+    throw new Error("Task to be deleted doesn't exist")
+  }
+}
